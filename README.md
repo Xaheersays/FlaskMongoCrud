3ea5393cd4684bfeb32ef9921ffd3eb6d2cc5f0f
unknown <shzaheer514@gmail.com>
2022-11-13 12:43:18 +0530

    readme

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 <h1>Fullstack Application ~ MongoDB CRUD Using Python with Flask</h1>
-<img >
+<h2>How to use</h2>
+$ git clone https://github.com/Xaheersays/FlaskMongoCrud.git
 
+<h2>Install Packages</h2>
+$ pip install flask
 
-            <form class="form-group" id="UpdateD">
-                <label class="text-light" for="fname">UPDATE  FROM Name </label>
-                <input type="text" name="fname" id="fname" class="form-control"><br>
-                <label class="text-light" for="tname">To  Name </label>
-                <input type="text" name="tname" id="tname" class="form-control"><br>
-                <button class="btn btn-dark btn btn-primary btn-lg" type="submit">Submit</button>
-            </form>
+$ pip install pymongo
+
+$ pip install flask_pymongo
+
+$ pip install Flask-Cors
+
+<h2>Download <a href="https://www.mongodb.com/try/download/community">MongoDB</a></h2>
+<h2>Download <a href="https://www.mongodb.com/try/download/shell">Mongosh </a>MongoShell</h2>
+<h2><a href="https://www.mongodb.com/docs/manual/installation/">here</a>is the detailed step by step instructions</h2>
+
+            
--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
-document.getElementById('DeleteForm').addEventListener('submit', DeleteData)
 
+
+document.getElementById('DeleteForm').addEventListener('submit', DeleteData)
 function DeleteData(e){
     e.preventDefault()
     let name = document.getElementById('dname').value;
@@ -20,18 +21,13 @@ function DeleteData(e){
 
 }
 
+
+
 document.getElementById('Updated').addEventListener('submit', UpdateData)
-console.log(document.getElementById('Updated'))
-
-
 function  UpdateData(e){
 e.preventDefault()
-
     let uname = document.getElementById('uname').value;
-        console.log(uname)
     let tname = document.getElementById('tname').value;
-        console.log(tname)
-
        fetch(`http://localhost:5000/update/${uname}/${tname}`, {
         method : 'PUT',
         headers : {
@@ -43,17 +39,11 @@ e.preventDefault()
     })
     .then((response) => response.json())
     .then((data) => console.log(data))
-      console.log("last")
 
 }
 
 
-
-
-
-
 document.getElementById('formData').addEventListener('submit', retrieveName)
-
 function retrieveName (e) {
     e.preventDefault()
 
@@ -65,13 +55,15 @@ function retrieveName (e) {
         name = data.name ?? "";
         genre = data.genre?? "";
         game  = data.game ?? "";
-//        console.log(name)
         document.getElementById('output').innerHTML = `<li class="list-group-item mx-auto shadow p-3 mb-5 bg-white rounded text-center" style="width: 200px;">Name : ${name}</li>
         <li class="list-group-item mx-auto shadow p-3 mb-5 bg-white rounded text-center" style="width: 200px;">Genre : ${genre}</li>
          <li class="list-group-item mx-auto  shadow p-3 mb-5 bg-white rounded text-center" style="width: 200px;">Game : ${game}</li>`
 
     })
 }
+
+
+
 
 document.getElementById('postData').addEventListener('submit', postData)
 function postData (e) {
@@ -93,5 +85,5 @@ function postData (e) {
         })
     })
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => console.log())
 }
